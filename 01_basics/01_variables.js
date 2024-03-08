aa8b012e493544b3f57269bf49ff5d9ab6ac3947
Abhishek Bansal <98174860+abhishekbansal2312@users.noreply.github.com>
2024-03-08 16:26:03 +0000

    dataypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 10101;
+let accountEmail = "abhi@gmail.com";
+var accountPass = "happy123";
+accountCity = "mumbai";
+let accountBranch;
+
+//  accountId=2;
+accountEmail="ban@gmail.com";
+accountPass = "123";
+accountCity="bilari";
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountBranch]);
+
+// dont use var because of issue in scope
+//const cannot be changed
+//let can be changed
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all js code as newer version of JS
+
+//alert(4+5);we are using nodeJS not JS
+
+console.log(typeof(null));
